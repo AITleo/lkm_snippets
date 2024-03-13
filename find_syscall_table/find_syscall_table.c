9b57ae73ba65f9f7dcc177e0e7e93b018b5e6dfc
root <root@local>
2024-03-13 15:29:28 +0100

    find syscall table

---
+++ find_syscall_table/find_syscall_table.c
@@ -0,0 +1,42 @@
+#include <linux/kprobes.h>
+#include <linux/printk.h>
+#include <linux/module.h>
+
+typedef unsigned long (*kallsyms_lookup_name_ptr)(const char *name);
+
+static struct kprobe kp = {
+    .symbol_name = "kallsyms_lookup_name"
+};
+
+kallsyms_lookup_name_ptr get_kallsyms_lookup_name(void) {
+    //unsigned long *syscall_table;
+
+    typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+    kallsyms_lookup_name_t kallsyms_lookup_name;
+    register_kprobe(&kp);
+    kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
+    unregister_kprobe(&kp);
+
+    //syscall_table = (unsigned long*)kallsyms_lookup_name("sys_call_table");
+    //return syscall_table;
+    
+    return kallsyms_lookup_name;
+}
+
+int init_module(void) {
+    unsigned long *syscall_table;
+    kallsyms_lookup_name_ptr kallsyms_lookup_name = get_kallsyms_lookup_name();
+    printk("kallsyms_lookup_name is at address %lx\n", (unsigned long)kallsyms_lookup_name);
+    syscall_table = (unsigned long*)kallsyms_lookup_name("sys_call_table");
+    printk("syscall_table is at address %lx\n", *syscall_table);
+    return 0;
+}
+
+void cleanup_module(void) {
+    printk("<1>Bye, Bye\n");
+}
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("leo");
+MODULE_DESCRIPTION("LKM get around unexported symbols");
